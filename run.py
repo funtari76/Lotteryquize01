71ffeed12f12d620a587e7ac88c045a790ec62a8
funtari76 <funtari777@gmail.com>
2022-05-17 19:38:14 +0000

    Have the basic structure of the game

--- run.py
+++ run.py
@@ -1,3 +1,21 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+def imagine (x):# create a var imagine and then pass it a random number 
+    random_number = random.randint(1, x)
+    imagine = 0
+    # User input
+    user = input("*****Please Enter You Name To Begin ->> ")
+    print("Well Done And Welcome", user, "This is a Lottery Guessing Lucky Numbers Game")
+    print("#########****Millions****########")
+    print("##-----------------------------##")
+    print("##-----------------------------##")
+    while imagine != random_number: # Create a while condition
+        imagine = int(input(f'Guess any number between 1 and {x}: '))
+        if imagine < random_number:
+           print('Eeeh ^o^! Sorry :(, try again. Too low.')
+        elif imagine > random_number:
+           print('Eeeh ^o^! Sorry :(, try again. Too high.')
+
+    print(f'Perfect! Well Done, You imagined the number {random_number} Exactly')    
+    
+   
